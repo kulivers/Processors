d407a060882485e199c6e4a01977ed4ed487a2ef
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2022-11-25 14:28:52 +0300

    new

--- Host/Program.cs
+++ Host/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        var agentYaml = @"D:\Work\CBAP_1\Processors\Host\config\agent.yaml";
+        var agentYaml = @"/home/asto/egor/Processors/Host/config/agent.yaml";
         var cfg = ConfigParser.Parse(agentYaml);
         var agent = new Agent(cfg);
         agent.Start();
